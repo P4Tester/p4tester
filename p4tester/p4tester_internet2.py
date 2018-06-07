dbf5649b2a5d69a1d8065220dfa1b64f6134d7df
Zhou Yu <yuz.thu@gmail.com>
2018-06-07 22:24:14 +0800

    first commit

---
+++ p4tester/p4tester_internet2.py
@@ -0,0 +1,19 @@
+from multiprocessing import Pool, cpu_count
+
+next_hop = {}
+router = {}
+
+
+def parse_internet2(file_list):
+    f = open(file_list)
+    for l in f:
+        l = l.strip('\n')
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
